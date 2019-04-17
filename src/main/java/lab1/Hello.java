a446528584d4fbe8422e9c00b136c98f9e44fc9a
Dylan Huntsman <huntsmd@wwu.edu>
2019-04-16 17:09:50 -0700

    Hello now prints command line arg

--- src/main/java/lab1/Hello.java
+++ src/main/java/lab1/Hello.java
@@ -2,6 +2,8 @@ package lab1;
 
 public class Hello{
     public static void main(String[] args){
-        System.out.println("Hello World!");
+	for (String arg : args) {        
+	System.out.println("Hello, " + arg + "!");
+	}
     }
 }
